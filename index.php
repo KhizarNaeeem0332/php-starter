9dbe646e1a202b0a2d2b36ab4e1cbb4b52f2e4b2
khizarnaeeem0332 <khizarnaeeem0332@gmail.com>
2018-06-18 20:51:50 +0500

    adding html

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('init.php');
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php include_once 'views/_tpl/header.php'?>
+    <?php include_once 'views/_tpl/style.php'?>
+
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <?php include_once 'views/_tpl/footer.php' ?>
+    <?php include_once 'views/_tpl/script.php' ?>
+  </body>
+</html>
---
+++ views/_tpl/script.php
@@ -0,0 +1,5 @@
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="public/js/jquery.min.js"></script>
+    <script src="public/js/popper.min.js"></script>
+    <script src="public/js/bootstrap.min.js"></script>
